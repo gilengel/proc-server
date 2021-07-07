95553accf29c228263645fedc8dd8f500e01e95c
gilengel <gil.engel@web.de>
2021-07-07 22:07:20 +0200

    Separate app and test into two files

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,7 @@ fn rocket() -> rocket::Rocket {
     rocket::ignite().mount("/", routes![hello])
 }
 
-fn main() {}
-
-#[cfg(test)]
-mod test {
-    use super::rocket;
-    use rocket::local::Client;
-    use rocket::http::Status;
-
-    #[test]
-    fn hello_world() {
-        let client = Client::new(rocket()).expect("valid rocket instance");
-        let mut response = client.get("/").dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.body_string(), Some("Hello, world!".into()));
-    }
+fn main() {
+    rocket();
 }
+
